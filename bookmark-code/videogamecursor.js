dd1f8d8078bd64928ea4ed2fefbbe6e157b7da30
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-06 08:48:42 -0500

    Update videogamecursor.js

--- bookmark-code/videogamecursor.js
+++ bookmark-code/videogamecursor.js
@@ -1,3 +1,3 @@
 var style = document.createElement("style");
-style.innerHTML="* { cursor: url('pinball3d.github.io/storage/videocursor.png'), move}";
+style.innerHTML="* { cursor: url('https://pinball3d.github.io/storage/videocursor.png'), move}";
 document.head.appendChild(style)
