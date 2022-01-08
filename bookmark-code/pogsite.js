116374d1dafa3c3bc44cc8db700b25df74600e5c
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-07 19:41:33 -0500

    Create pogsite.js

---
+++ bookmark-code/pogsite.js
@@ -0,0 +1,5 @@
+document.body.innerHTML="";
+var div = document.createElement("div");
+div.innerHTML="POG";
+div.style="position: absolute; top: 50%; left: 50%; margin: -25px 0 0 -25px; font-family: Arial; font-weight: bold;";
+document.body.appendChild(div);
