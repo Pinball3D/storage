0aca04e27c67f3ba282f994cb66b2a9437f00d05
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-08 23:42:57 -0500

    Update alarm.js

--- bookmark-code/alarm.js
+++ bookmark-code/alarm.js
@@ -2,6 +2,6 @@ var e = new Audio('https://pinball3d.github.io/storage/Samsung_GALAXY_S4_Alarms_
 e.play();
 document.body.onkeyup = function(key) {
 if (key.keyCode == 80) {
-  var e = new Audio('https://pinball3d.github.io/storage/Samsung_GALAXY_S4_Alarms_-_Morning_Flower.mp3');
-  e.play();
+  var a = new Audio('https://pinball3d.github.io/storage/Samsung_GALAXY_S4_Alarms_-_Morning_Flower.mp3');
+  a.play();
 }
