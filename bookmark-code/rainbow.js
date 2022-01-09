38625b4fb5a30c96d4aa7b8c6bc6169e0cff14dc
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-08 23:11:07 -0500

    Create rainbow.js

---
+++ bookmark-code/rainbow.js
@@ -0,0 +1,10 @@
+var head=document.head;
+var link=document.createElement("link");
+
+link.type="text/css";
+link.rel="stylesheet";
+link.href="https://pinball3d.github.io/storage/rainbow.css";
+
+head.appendChild(link);
+document.body.style.animation="bgColor5sinfinitelinear";
+document.getElementById("kO001e").style.animation="bgColor5sinfinitelinear";
