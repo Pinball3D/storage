01f3a4d7669b9fb08808edb967ef940747496bc5
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-08 23:28:34 -0500

    Create mcoof.js

---
+++ bookmark-code/mcoof.js
@@ -0,0 +1,8 @@
+var url = "https://pinball3d.github.io/storage/oof.mp3";
+var o = new Audio(url);
+o.play();
+document.body.onkeyup = function(key) {
+if (key.keyCode==80) {
+var audio = new Audio(url);
+audio.play();
+}
