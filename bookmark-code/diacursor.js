8df850a555d95ca2349ae96ce02d693bdba350b2
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-08 23:05:34 -0500

    Create diacursor.js

---
+++ bookmark-code/diacursor.js
@@ -0,0 +1,3 @@
+var style = document.createElement("style");
+style.innerHTML="* { cursor: url('https://github.com/Pinball3D/storage/blob/main/diamond%20sword.cur?raw=true'), move}";
+document.head.appendChild(style);
