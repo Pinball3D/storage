f8cab41b2f9b3cebcdc13e7d6963cb1147b09e8d
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-19 22:03:33 -0500

    Update rickroll.js

--- bookmark-code/rickroll.js
+++ bookmark-code/rickroll.js
@@ -2,6 +2,7 @@ var b = new Audio('https://pinball3d.github.io/storage/rick.mp3');
 b.play();
 document.body.onkeyup = function(key) {
 if (key.keyCode==80) {
-  var b = new Audio('https://pinball3d.github.io/storage/rick.mp3');
-  b.play();
+  var o = new Audio('https://pinball3d.github.io/storage/rick.mp3');
+  o.play();
+}
 }
