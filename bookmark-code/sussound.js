027aab3594445678d5b395801a15790ed3816934
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-19 22:07:48 -0500

    Update sussound.js

--- bookmark-code/sussound.js
+++ bookmark-code/sussound.js
@@ -1,6 +1,7 @@
 var sound = new Audio('https://pinball3d.github.io/storage/yt5s.com%20-%20when%20the%20imposter%20is%20sus%20meme%20download%20mp3%20free%20(320%20kbps)%20(1).mp3');
 sound.play();
 document.body.onkeyup = function(key) {if (key.keyCode == 80) {
-  var sound = new Audio('https://pinball3d.github.io/storage/yt5s.com%20-%20when%20the%20imposter%20is%20sus%20meme%20download%20mp3%20free%20(320%20kbps)%20(1).mp3');       
-  sound.play();
+  var s = new Audio('https://pinball3d.github.io/storage/yt5s.com%20-%20when%20the%20imposter%20is%20sus%20meme%20download%20mp3%20free%20(320%20kbps)%20(1).mp3');       
+  s.play();
+}
 }
