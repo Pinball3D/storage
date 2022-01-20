3f45a45e8f72c5e0ed6e72ded8003e58f85613ed
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-19 22:05:33 -0500

    Update androidnotif.js

--- bookmark-code/androidnotif.js
+++ bookmark-code/androidnotif.js
@@ -2,6 +2,7 @@ var o = new Audio('https://pinball3d.github.io/storage/aindroid.mp3');
 o.play();
 document.body.onkeyup = function(key) {
 if (key.keyCode == 80) {
-  var o = new Audio('https://pinball3d.github.io/storage/aindroid.mp3');
-  o.play();
+  var a = new Audio('https://pinball3d.github.io/storage/aindroid.mp3');
+  a.play();
+}
 }
