a6bac1a2ef1f087f7b87867eb2a101815f0a279f
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-25 12:50:27 -0500

    Create allvineboom.js

---
+++ bookmark-code/allvineboom.js
@@ -0,0 +1,6 @@
+var sound = new Audio('https://pinball3d.github.io/storage/Vine_Boom_Sound_Effect.mp3');
+sound.play();
+document.body.onkeyup = function(key) {
+  var s = new Audio('https://pinball3d.github.io/storage/Vine_Boom_Sound_Effect.mp3');
+  s.play();
+}
