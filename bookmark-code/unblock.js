1d48cb095defe93d58cf8b4ef73a371dca4055b0
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-03-07 20:11:09 -0500

    Update unblock.js

--- bookmark-code/unblock.js
+++ bookmark-code/unblock.js
@@ -1,4 +1,5 @@
 var req = new XMLHttpRequest();
 req.open('GET','https://unblockerserver.herokuapp.com/?url='+document.location.href,false);
 req.send('');
+console.log(req.status);
 document.write(req.responseText);
