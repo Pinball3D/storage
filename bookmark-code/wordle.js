495e587c3de0f6e27d8f8a1d22905fde5f0618c4
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-05-24 22:08:01 -0400

    Update wordle.js

--- bookmark-code/wordle.js
+++ bookmark-code/wordle.js
@@ -1,7 +1,6 @@
 var abc, e, fivel, fourl, numOfLetters, onel, r, sixl, threel, twol, word;
 e = " " + prompt("Word Scramble: ");
 e = e.split("");
-numOfLetters = e.length;
 word = "";
 abc = "abcdefghijklmnopqrstuvwxyz";
 abc = abc.split("");
@@ -11,7 +10,6 @@ threel = ["r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", 
 fourl = ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"];
 fivel = ["l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
 sixl = ["e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d"];
-r = range(numOfLetters);
 
 for (var i in e) {
 
@@ -22,32 +20,32 @@ for (var i in e) {
   console.log("hi" + i.toString());
 
   if (i === 1 || i === 7 || i === 13 || i === 19 || i === 25 || i === 31) {
-    word = word + abc[onel.index(e[i])];
+    word = word + abc[onel.indexOf(e[i])];
     continue;
   }
 
   if (i === 2 || i === 8 || i === 14 || i === 20 || i === 26 || i === 32) {
-    word = word + abc[twol.index(e[i])];
+    word = word + abc[twol.indexOf(e[i])];
     continue;
   }
 
   if (i === 3 || i === 9 || i === 15 || i === 21 || i === 27 || i === 33) {
-    word = word + abc[threel.index(e[i])];
+    word = word + abc[threel.indexOf(e[i])];
     continue;
   }
 
   if (i === 4 || i === 9 || i === 16 || i === 22 || i === 28 || i === 34) {
-    word = word + abc[fourl.index(e[i])];
+    word = word + abc[fourl.indexOf(e[i])];
     continue;
   }
 
   if (i === 5 || i === 9 || i === 17 || i === 23 || i === 29 || i === 35) {
-    word = word + abc[fivel.index(e[i])];
+    word = word + abc[fivel.indexOf(e[i])];
     continue;
   }
 
   if (i === 6 || i === 9 || i === 18 || i === 24 || i === 30 || i === 36) {
-    word = word + abc[sixl.index(e[i])];
+    word = word + abc[sixl.indexOf(e[i])];
     continue;
   }
 }
