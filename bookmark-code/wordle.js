51d81faf466779b3a4001e866492b72410e6224f
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-05-24 22:05:12 -0400

    Create wordle.js

---
+++ bookmark-code/wordle.js
@@ -0,0 +1,55 @@
+var abc, e, fivel, fourl, numOfLetters, onel, r, sixl, threel, twol, word;
+e = " " + prompt("Word Scramble: ");
+e = e.split("");
+numOfLetters = e.length;
+word = "";
+abc = "abcdefghijklmnopqrstuvwxyz";
+abc = abc.split("");
+onel = ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"];
+twol = ["o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
+threel = ["r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"];
+fourl = ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"];
+fivel = ["l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
+sixl = ["e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d"];
+r = range(numOfLetters);
+
+for (var i in e) {
+
+  if (i === 0) {
+    continue;
+  }
+
+  console.log("hi" + i.toString());
+
+  if (i === 1 || i === 7 || i === 13 || i === 19 || i === 25 || i === 31) {
+    word = word + abc[onel.index(e[i])];
+    continue;
+  }
+
+  if (i === 2 || i === 8 || i === 14 || i === 20 || i === 26 || i === 32) {
+    word = word + abc[twol.index(e[i])];
+    continue;
+  }
+
+  if (i === 3 || i === 9 || i === 15 || i === 21 || i === 27 || i === 33) {
+    word = word + abc[threel.index(e[i])];
+    continue;
+  }
+
+  if (i === 4 || i === 9 || i === 16 || i === 22 || i === 28 || i === 34) {
+    word = word + abc[fourl.index(e[i])];
+    continue;
+  }
+
+  if (i === 5 || i === 9 || i === 17 || i === 23 || i === 29 || i === 35) {
+    word = word + abc[fivel.index(e[i])];
+    continue;
+  }
+
+  if (i === 6 || i === 9 || i === 18 || i === 24 || i === 30 || i === 36) {
+    word = word + abc[sixl.index(e[i])];
+    continue;
+  }
+}
+
+console.log(word);
