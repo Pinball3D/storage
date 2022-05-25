9ba961fc44b3d4c9ddc27829e1ca904c84191df5
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-05-24 22:09:56 -0400

    Update wordle.js

--- bookmark-code/wordle.js
+++ bookmark-code/wordle.js
@@ -20,31 +20,37 @@ for (var i in e) {
   console.log("hi" + i.toString());
 
   if (i === 1 || i === 7 || i === 13 || i === 19 || i === 25 || i === 31) {
+    console.log(abc[onel.indexOf(e[i])]);
     word = word + abc[onel.indexOf(e[i])];
     continue;
   }
 
   if (i === 2 || i === 8 || i === 14 || i === 20 || i === 26 || i === 32) {
+    console.log(abc[twol.indexOf(e[i])]);
     word = word + abc[twol.indexOf(e[i])];
     continue;
   }
 
   if (i === 3 || i === 9 || i === 15 || i === 21 || i === 27 || i === 33) {
+    console.log(abc[threel.indexOf(e[i])]);
     word = word + abc[threel.indexOf(e[i])];
     continue;
   }
 
   if (i === 4 || i === 9 || i === 16 || i === 22 || i === 28 || i === 34) {
+    console.log(abc[fourl.indexOf(e[i])]);
     word = word + abc[fourl.indexOf(e[i])];
     continue;
   }
 
   if (i === 5 || i === 9 || i === 17 || i === 23 || i === 29 || i === 35) {
+    console.log(abc[fivel.indexOf(e[i])]);
     word = word + abc[fivel.indexOf(e[i])];
     continue;
   }
 
   if (i === 6 || i === 9 || i === 18 || i === 24 || i === 30 || i === 36) {
+    console.log(abc[sixl.indexOf(e[i])]);
     word = word + abc[sixl.indexOf(e[i])];
     continue;
   }
