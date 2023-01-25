b8f595206d82097f03036bc3cdbca0d272d598c7
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2023-01-25 17:57:31 -0500

    Update rickroll.js

--- bookmark-code/rickroll.js
+++ bookmark-code/rickroll.js
@@ -1,8 +1,12 @@
-var b = new Audio('https://pinball3d.github.io/storage/rick.mp3');
-b.play();
+var b = document.createElement("audio");
+b.src = 'https://pinball3d.github.io/storage/rick.mp3';
+b.autoplay="autoplay"
+document.body.appendChild(b);
 document.body.onkeyup = function(key) {
 if (key.keyCode==80) {
-  var o = new Audio('https://pinball3d.github.io/storage/rick.mp3');
-  o.play();
-}
+  var b = document.createElement("audio");
+  b.src = 'https://pinball3d.github.io/storage/rick.mp3';
+  b.autoplay="autoplay"
+  document.body.appendChild(b);
+ }
 }
