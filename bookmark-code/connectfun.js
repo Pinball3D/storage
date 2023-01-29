655aeff6b87def3d65d918b1a7855058ff347adf
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2023-01-29 14:56:12 -0500

    Update connectfun.js

--- bookmark-code/connectfun.js
+++ bookmark-code/connectfun.js
@@ -1,24 +1,31 @@
-var body = "";
-document.body.innerHTML="";
-ws = new WebSocket("wss://5065322b3d2b440f91c9a7a6690de1b7.vfs.cloud9.us-east-1.amazonaws.com");
-ws.onopen = function() {
-  console.log("opened");
-};
-				
-ws.onmessage = function (evt) { 
-  var received_msg = evt.data;
-  document.body.innerHTML = evt.data;
-  body = document.body.innerHTML
-};
-				
-ws.onclose = function() { 
-  console.log("Closed")
-};
-setInterval(function() { counter() }, 0);
-function counter()
-{
-  if (document.body.innerHTML != body) {
-    ws.send(document.body.innerHTML)
-    body = document.body.innerHTML;
-  }
+if (window.location.href == "about:blank") {
+    var body = "";
+    document.body.innerHTML = "";
+    ws = new WebSocket("ws://ec2-18-205-105-185.compute-1.amazonaws.com:5000/");
+    ws.onopen = function() {
+        console.log("opened");
+    };
+
+    ws.onmessage = function(evt) {
+        var received_msg = evt.data;
+        document.body.innerHTML = evt.data;
+        body = document.body.innerHTML
+    };
+
+    ws.onclose = function() {
+        console.log("Closed")
+    };
+    setInterval(function() {
+        counter()
+    }, 0);
+
+    function counter() {
+        if (document.body.innerHTML != body) {
+            ws.send(document.body.innerHTML)
+            body = document.body.innerHTML;
+        }
+    }
+} else {
+    alert("IMPORTANT: When you click ok, you will be brought to a new page. When you get to the new page, click this bookmark again.");
+    window.location.href = "about:blank";
 }
