ce6f6476d0c91c6b096c7a06b480d486b6be3ca5
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2024-09-23 12:24:02 -0400

    Create skibidiprincipal.js

---
+++ skibidiprincipal.js
@@ -0,0 +1,9 @@
+const newWindow = window.open('/NHSPrincipal');
+
+// Wait for the new window to fully load before running the script
+newWindow.onload = function() {
+  // Execute JavaScript in the new window's context
+  // newWindow.document.body.style.backgroundColor = 'yellow';
+    newWindow.document.querySelector("img[src='/_theme/images/Klong.png']").src="https://imgix.bustle.com/uploads/image/2024/7/24/fb89d756/screen-shot-2024-07.png?w=320&h=525&fit=crop&crop=faces&q=50&dpr=2"
+  newWindow.console.log('New window loaded and script executed!');
+};
